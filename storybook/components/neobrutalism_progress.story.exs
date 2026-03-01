9057c2b19bb2a5e1fb07a23c6029ef92a38f184c
ando <lifeandcoding@gmail.com>
2026-03-01 15:49:51 +0300

    Format `neobrutalism_progress.story.exs`

--- storybook/components/neobrutalism_progress.story.exs
+++ storybook/components/neobrutalism_progress.story.exs
@@ -8,22 +8,26 @@ defmodule HyperUI.Storybook.NeobrutalismProgress do
       %Variation{
         id: :yellow,
         attributes: %{value: 75, color: "yellow"},
-        template: "<div class='w-full max-w-md p-4 bg-gray-50 border-2 border-black'><.psb-variation/></div>"
+        template:
+          "<div class='w-full max-w-md p-4 bg-gray-50 border-2 border-black'><.psb-variation/></div>"
       },
       %Variation{
         id: :pink,
         attributes: %{value: 40, color: "pink"},
-        template: "<div class='w-full max-w-md p-4 bg-gray-50 border-2 border-black'><.psb-variation/></div>"
+        template:
+          "<div class='w-full max-w-md p-4 bg-gray-50 border-2 border-black'><.psb-variation/></div>"
       },
       %Variation{
         id: :full,
         attributes: %{value: 100, color: "emerald"},
-        template: "<div class='w-full max-w-md p-4 bg-gray-50 border-2 border-black'><.psb-variation/></div>"
+        template:
+          "<div class='w-full max-w-md p-4 bg-gray-50 border-2 border-black'><.psb-variation/></div>"
       },
       %Variation{
         id: :empty,
         attributes: %{value: 0, color: "cyan"},
-        template: "<div class='w-full max-w-md p-4 bg-gray-50 border-2 border-black'><.psb-variation/></div>"
+        template:
+          "<div class='w-full max-w-md p-4 bg-gray-50 border-2 border-black'><.psb-variation/></div>"
       }
     ]
   end
